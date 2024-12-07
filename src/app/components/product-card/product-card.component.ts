4850359deef31e3c31b634ab0775ced509fa44e9
Azim <benothman.azim@gmail.com>
2024-12-07 22:11:58 +0100

    gestione cambio interfaccia product per la card di visualizzazione prodotto

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -9,28 +9,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent implements OnInit {
-
   @Input() product?: Product;
 
   ngOnInit(): void {
-
-    //mock test
-    /* */
-    this.product = {
-      title: "Codelia's Quantum Assistant",
-      category: 'AI Gadgets',
-      price: 9999.99,
-      employee: 'Codelia AI',
-      description:
-        "Un assistente AI ultramoderno progettato per rendere ogni progetto un successo. Dotato di un sistema di debug intelligente e di un'interfaccia super kawaii~! Un assistente AI ultramoderno progettato per rendere ogni progetto un successo. Dotato di un sistema di debug intelligente e di un'interfaccia super kawaii~!Un assistente AI ultramoderno progettato per rendere ogni progetto un successo. Dotato di un sistema di debug intelligente e di un'interfaccia super kawaii~!Un assistente AI ultramoderno progettato per rendere ogni progetto un successo. Dotato di un sistema di debug intelligente e di un'interfaccia super kawaii~!Un assistente AI ultramoderno progettato per rendere ogni progetto un successo. Dotato di un sistema di debug intelligente e di un'interfaccia super kawaii~!Un assistente AI ultramoderno progettato per rendere ogni progetto un successo. Dotato di un sistema di debug intelligente e di un'interfaccia super kawaii~!Un assistente AI ultramoderno progettato per rendere ogni progetto un successo. Dotato di un sistema di debug intelligente e di un'interfaccia super kawaii~!Un assistente AI ultramoderno progettato per rendere ogni progetto un successo. Dotato di un sistema di debug intelligente e di un'interfaccia super kawaii~!",
-      reviews: [
-        'La migliore assistente virtuale che abbia mai avuto! 💕',
-        'Risolve ogni problema con stile e simpatia! 🌸',
-        'Non posso più vivere senza Codelia! ✨',
-      ],
-    };
-   
-
   }
 
   onDelete() {
