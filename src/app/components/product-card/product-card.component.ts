1e9657111a5ce5b8d131591ce51f7f27b3f2d449
Azim <benothman.azim@gmail.com>
2024-12-09 23:06:12 +0100

    aggiunta visualizzazione recensioni

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,17 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../../interfaces/product.interface';
 import { CommonModule } from '@angular/common';
+import { ReviewsComponent } from "../reviews/reviews.component";
 
 @Component({
   selector: 'app-product-card',
-  imports: [CommonModule],
+  imports: [CommonModule, ReviewsComponent],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent implements OnInit {
   @Input() product?: Product;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    
+  }
 
   onDelete() {
     console.log('onDelete');
