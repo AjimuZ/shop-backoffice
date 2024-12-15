1e0983b6d6a00a5b8822ee1b62886fe6e7ff83ee
Azim <benothman.azim@gmail.com>
2024-12-15 20:41:48 +0100

    creazione grafico

--- src/app/pages/chart/chart.component.ts
+++ src/app/pages/chart/chart.component.ts
@@ -2,6 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { StatsCategories } from '../../interfaces/stats-categories.interface';
 import { ChartService } from './chart.service';
+import {
+  Chart,
+  ArcElement,
+  Tooltip,
+  Legend,
+  PolarAreaController,
+  RadialLinearScale,
+  Title,
+} from 'chart.js';
 
 @Component({
   selector: 'app-chart',
@@ -12,10 +21,67 @@ import { ChartService } from './chart.service';
 export class ChartComponent implements OnInit {
   statsCategories: StatsCategories[] = [];
 
-  constructor(private chartService: ChartService) {}
+  constructor(private chartService: ChartService) {
+    Chart.register(
+      ArcElement,
+      Tooltip,
+      Legend,
+      PolarAreaController,
+      RadialLinearScale,
+      Title
+    );
+  }
 
   async ngOnInit(): Promise<void> {
     await this.chartService.loadProducts(this.statsCategories);
-    console.log(this.statsCategories);
+    this.createChart();
   }
+
+  createChart(): void {
+    const categories = this.statsCategories.map((item) => item.category);
+    const productCounts = this.statsCategories.map(
+      (item) => item.numberOfProducts
+    );
+
+    new Chart('productsChart', {
+      type: 'polarArea',
+      data: {
+        labels: categories,
+        datasets: [
+          {
+            label: 'Products per Category',
+            data: productCounts,
+            backgroundColor: [
+              '#FF6384',
+              '#36A2EB',
+              '#FFCE56',
+              '#4BC0C0',
+              '#9966FF',
+              '#FF9F40',
+              '#C9CBCF',
+              '#FF6F61',
+              '#F7B7A3',
+              '#B8E986',
+            ],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'right',
+          },
+          title: {
+            display: true,
+            text: 'Distribution of Products by Category',
+            align: 'end',
+          },
+        },
+      },
+    });
+  }
+
+  // lifecycle: ensures the price input field is focused after each view check
+  ngAfterViewChecked(): void {}
 }
