700deead71f471c7be236b8e0ff5715882afd592
Azim <benothman.azim@gmail.com>
2024-12-15 18:24:58 +0100

    aggiunta spinner durante le chiamate http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgHttpLoaderComponent } from 'ng-http-loader';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgHttpLoaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'shop-backoffice';
