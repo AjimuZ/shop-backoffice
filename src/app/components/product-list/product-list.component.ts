813518d31e163612156a11893d90fae7e24bd2e7
Azim <benothman.azim@gmail.com>
2024-12-17 01:23:43 +0100

    aggiunta paginazione per i prodotti

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,9 +1,13 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  Input,
+  SimpleChanges,
+} from '@angular/core';
 import { Product } from '../../interfaces/product.interface';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { CommonModule } from '@angular/common';
 import { LayoutType } from '../../enums/layout-type';
-import { ProductListService } from './product-list.service';
 
 @Component({
   selector: 'app-product-list',
@@ -15,5 +19,48 @@ export class ProductListComponent {
   @Input() products?: Product[] = [];
   @Input() layout?: LayoutType;
 
-  constructor(private productListService: ProductListService) {}
+  currentPage = 1;
+  itemsPerPage = 6;
+  pages: number[] = [];
+
+  ngOnInit(): void {
+    this.calculatePages();
+  }
+
+  ngOnChanges(): void {
+    if (this.products && this.products.length > 0) {
+      this.calculatePages();
+    }
+  }
+
+  ngDoCheck(): void {
+    if (this.products && this.products.length > 0) {
+      this.calculatePages();
+
+      // if you are out of limit, reset first page
+      const maxPage = Math.ceil(this.products.length / this.itemsPerPage);
+      if (this.currentPage > maxPage) {
+        this.currentPage = 1;
+      }
+    }
+  }
+
+  onPageChange(page: number): void {
+    if (page >= 1 && page <= this.pages.length) {
+      this.currentPage = page;
+    }
+  }
+
+  calculatePages(): void {
+    if (this.products?.length) {
+      const totalPages = Math.ceil(this.products.length / this.itemsPerPage);
+      this.pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+    }
+  }
+
+  paginatedProducts(): Product[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.products?.slice(startIndex, endIndex) || [];
+  }
 }
